4fd3fd8153508e62ac36414a230038bdcb34c7dc
Oguzhan Kabul <102929380+oguzhankabul@users.noreply.github.com>
2024-04-07 20:19:12 +0300

    Created LocationManager, TrackMapViewController UI
    - Created LocationManager and location logic
    - TrackMapViewController configured
    - Start Tracking, Stop Tracking and Reset Tracking actions added

---
+++ TrackLocation/TrackLocation/Utilities/Extensions/Int+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Int+Extension.swift
+//  TrackLocation
+//
+//  Created by Oğuzhan Kabul on 7.04.2024.
+//
+
+import Foundation
+
+extension Int {
+    func toString() -> String {
+        return String(self)
+    }
+}
